a1cead718f53aace1d3cbaba8bbda562557306d4
DESKTOP-TEOCFN4\Usuario <jeroenvhaasteren@gmail.com>
2017-02-12 13:42:31 +0100

    Initial Commit

---
+++ src/main/Order.java
@@ -0,0 +1,16 @@
+package main;
+
+import pablo.Dish;
+
+public class Order extends Dish {
+
+	private String m_customerName;
+	//private Dish m_dish;
+	private int m_type;
+	private boolean m_gfd;
+	private boolean m_vgd;
+	private boolean m_hmd;
+	private boolean m_sfd;
+	private String Extras;
+	
+}
